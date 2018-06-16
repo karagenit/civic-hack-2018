cf2af2bd8c49ff4677a664fd018775325149154b
Armaan Goel <armaangoel78@gmail.com>
2018-06-16 03:04:28 -0400

    Fixed Sams dumbassery

--- businesses/urls.py
+++ businesses/urls.py
@@ -6,15 +6,11 @@ from django.views.generic import ListView, DetailView
 from . import views
 
 urlpatterns = [
-<<<<<<< HEAD
-=======
-
     url(r'^viewItems/(?P<biz_id>[0-9]+)', views.viewItems),
     url(r'^addFoodItems/(?P<biz_id>[0-9]+)', views.addFoodItem),
     url(r'^addToItemCount/(?P<item_id>[0-9]+)', views.addToItemCount),
     url(r'^subtractFromItemCount/(?P<item_id>[0-9]+)', views.subtractFromItemCount),
     url(r'^restaurantfoods/', views.restaurantfoods),
->>>>>>> e59628fe03a7e410b7d67bcb5868e80e70636c7e
     url(r'^$', views.home),
     url(r'^test/$', views.test),
     url(r'^viewItems/(?P<biz_id>[0-9]+)/$', views.viewItems),

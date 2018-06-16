853edc349d27190e1cd13ef753186bebd51b58e4
Armaan Goel <armaangoel78@gmail.com>
2018-06-15 20:47:13 -0400

    set up settings, static files, resources

--- OldFood/settings.py
+++ OldFood/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'OldFood.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['resources/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    #'/var/www/static/',
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
---
+++ OldFood/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render, redirect
+
+
+# Sending user object to the form, to verify which fields to display/remove (depending on group)
+def home(request):
+    return render(request, 'hi.html', {})

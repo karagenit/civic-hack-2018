f947a81d7cceafe587e75a1011851b66a70d0709
Caleb Smith <karagenit@outlook.com>
2018-06-15 21:41:23 -0400

    Add Index File

--- OldFood/views.py
+++ OldFood/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render, redirect
 
 # Sending user object to the form, to verify which fields to display/remove (depending on group)
 def home(request):
-    return render(request, 'hi.html', {'age': 72, })
+    return render(request, 'index.html')

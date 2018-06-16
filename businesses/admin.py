828adb6126698c9436f0101918bcce899d8d3faf
Evan Kenyon <evan.kenyon123@gmail.com>
2018-06-16 00:02:25 -0400

    Made Businesses one-to-one with profiles

--- businesses/admin.py
+++ businesses/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Business
 
-# Register your models here.
+admin.site.register(Business)
--- businesses/models.py
+++ businesses/models.py
@@ -1,10 +1,27 @@
 from django.db import models
-
+from accounts.models import Profile
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 # Create your models here.
 
 class Business(models.Model):
     name = models.CharField(max_length=256)
     address = models.CharField(max_length=256)
+    profile = models.OneToOneField(Profile, on_delete=models.CASCADE, default=None)
+
+    @receiver(post_save, sender=Profile)
+    def create_user_business(sender, instance, created, **kwargs):
+        if created:
+            Business.objects.create(profile=instance)
+
+    @receiver(post_save, sender=Profile)
+    def save_user_business(sender, instance, **kwargs):
+        #TODO: should be able to delete all the except: stuff after all the users have run this
+        if instance.member_type == '3':
+            instance.business.save()
+
+
 
 class PickupRequest(models.Model):
     business = models.ForeignKey('Business', on_delete=models.CASCADE)

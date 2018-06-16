824357c04dde29b58ac293f9cb5bb6e12741e597
Armaan Goel <armaangoel78@gmail.com>
2018-06-16 03:36:08 -0400

    fixed login redirect issue

--- OldFood/settings.py
+++ OldFood/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-LOGIN_REDIRECT_URL = ''
+LOGIN_REDIRECT_URL = '/'
 
 
 STATIC_URL = '/static/'

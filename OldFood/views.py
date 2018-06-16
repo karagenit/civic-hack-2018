c50916790c05d2ca421d1f7630006b6c4cde0167
jsenuta <johnbikerun@gmail.com>
2018-06-16 07:04:38 -0400

    Draft landing page

--- OldFood/views.py
+++ OldFood/views.py
@@ -3,16 +3,4 @@ from django.shortcuts import render, redirect
 
 # Sending user object to the form, to verify which fields to display/remove (depending on group)
 def home(request):
-    if request.user.is_authenticated():
-        user = request.user
-        if (user.profile.is_client()):
-            return redirect('/client')
-        elif (user.profile.is_restaurant()):
-            return redirect('/businesses')
-        elif (user.profile.is_driver()):
-            return redirect('/volunteers')
-        else:
-            return redirect('/login')
-
-    else:
-        return redirect('/login')
+    return render(request, 'index.html')
